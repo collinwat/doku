c013c3234eb7e98600f674ce06b83de3f5fd99e1
Collin Watson <collinwat@gmail.com>
2013-04-16 12:18:24 -0700

    Ignoring common local files

--- doku/sudoku/parse.py
+++ doku/sudoku/parse.py
@@ -1,3 +1,6 @@
+import utils
+
+
 class UnknownCharacterError(ValueError):
     FORMAT = 'Parser does not know how to handle the "%s" character'
 
@@ -7,14 +10,59 @@ class UnknownCharacterError(ValueError):
 
 class StringParser(object):
 
-    def parse(self, line):
-        if not line or len(line) < 81:
+    def __init__(self, size):
+        """
+        This parser expects that strings only contain numberic characters and
+        '.' to represent an empty cell. It assumes that each character
+        represents a cell.
+
+        Here is a 4x4 example:
+            2..1....4.1.31.2
+
+        Due to these assumptions, the only acceptable sudoku perfect square
+        sizes are 4 and 9, since they do not require a number larger than a
+        single digit.
+        """
+
+        if size != 4 and size != 9:
+            msg = 'The size "%s" is invalid. Size can only be 4 or 9.'
+            raise ValueError(msg, size)
+
+        self.size = size
+        self.length = size * size
+        self.box_size = utils.box_size(size)
+
+    def iparse(self, line):
+        if not line or len(line) < self.length:
             return
-        return [self.parse_char(c) for c in line[0:81]]
+        row = 0
+        column = 0
+        count = 0
+
+        for c in line:
+            c = c.strip()
+
+            if c == '':
+                continue
+
+            number = self.parse_char(c)
+
+            if number:
+                yield (row, column, number - 1)
+
+            count += 1
+            if column == self.size - 1:
+                row += 1
+                column = 1
+            else:
+                column += 1
+
+            if count == self.length:
+                return
 
     def parse_char(self, value):
-        if not value or value == '.':
-            return '.'
+        if value == '.':
+            return
 
         result = self.parse_int(value)
 
